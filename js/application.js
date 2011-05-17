0355be6e2d3df69e5b8040fe574a21b261a535ee
Oliver Schrenk <oliver.schrenk@gmail.com>
2011-05-18 01:43:14 +0200

    First working version

--- js/application.js
+++ js/application.js
@@ -3,34 +3,27 @@ $(document).ready(function() {
 	var Option = Backbone.Model.extend({
 		defaults: {
 			"value": undefined,
-			"path": undefined,
-			"active": false
+			"path": undefined
 		}
 	});
 
 	var Options = Backbone.Collection.extend({
 		model: Option,
-		//localStorage: new Store("todos"),
-		current: function() {
-			return this.filter(function(todo) {
-				return todo.get('active');
-			});
-		},
 		url: 'options'
 	});
 
 	var OptionGroup = Backbone.Model.extend({
 		defaults: {
 			"id": undefined,
-			"options": undefined
+			"options": undefined,
+			"active": undefined
 		},
 		current: function() {
-			return this.get('options').current();
+			return this.get('active');
 		},
 		size: function() {
 			return this.get('options').size();
 		}
-
 	});
 
 	var OptionGroupView = Backbone.View.extend({
@@ -49,7 +42,7 @@ $(document).ready(function() {
 			"selectedBoxColor": "#585858",
 			"notSelectedBoxColor": "#f6f7f6"
 		},
-		boxes: [],
+		boxes: {},
 		events: {},
 		initialize: function() {
 			var x = this.options['x'];
@@ -65,28 +58,6 @@ $(document).ready(function() {
 				stroke: this.options["strokeColor"]
 			};
 
-			var clicker = function() {
-				if (this.model.get('options').models[i].get('active') === true) {
-					this.model.set('options').models[i].get('active') = false;
-					boxes[i].attr({
-						fill: this.options["notSelectedBoxColor"]
-					});
-				} else {
-					for (j = 0; j < numberOfBoxes; j++) {
-						if (j == i) {
-							boxes[j].attr({
-								fill: this.options["selectedBoxColor"]
-							});
-						} else {
-							boxes[j].attr({
-								fill: this.options["notSelectedBoxColor"]
-							});
-						}
-					}
-					this.model.select(i);
-				};
-			};
-
 			for (var i = 0; i < numberOfBoxes; i++) {
 				var isFirst = (i == 0);
 				var isSelected = (this.model.get('options').models[i].get('active') === true);
@@ -96,23 +67,35 @@ $(document).ready(function() {
 				var trRadius = isLast ? this.options["radius"] : 0;
 				var brRadius = isLast ? this.options["radius"] : 0;
 				var blRadius = isFirst ? this.options["radius"] : 0;
+				var diff = this.options["x"] + i * this.options["boxWidth"] + (this.options["boxWidth"] - this.options["iconWidth"]) / 2;
 
-				this.boxes[i] = paper.roundedRect(
+				var option = this.model.get('options').models[i];
+				var optionValue = option.get('value');
+				
+				// underlying rounded rectangle indicating state
+				this.boxes[optionValue] = paper.roundedRect(
 				this.options["x"] + i * this.options["boxWidth"], this.options["y"], this.options["boxWidth"], this.options["boxHeight"], tlRadius, trRadius, brRadius, blRadius).attr({
 					fill: isSelected ? this.options["selectedBoxColor"] : this.options["notSelectedBoxColor"],
 					"stroke": this.options["strokeColor"],
 					"stroke-width": this.options["strokeThickness"]
 				});
 
-				paper.path(this.model.get('options').models[i].get('path')).attr(opaque).translate(
-				this.options["x"] + i * this.options["boxWidth"] + (this.options["boxWidth"] - this.options["iconWidth"]) / 2, this.options["y"] + (this.options["boxHeight"] - this.options["iconHeight"]) / 2);
+				// symbol
+				paper.path(option.get('path')).attr(opaque).translate(diff, 0);
+			
+				// transparent clickable box
+				var transparentBox = paper.rect(this.options["x"] + i * this.options["boxWidth"], this.options["y"], this.options["boxWidth"], this.options["boxHeight"]).attr(transparent);
+				$(transparentBox.node).attr("class", "picker " + "id." + this.model.id + " value." + option.get('value'));	
 			}
 		},
-		change: function() {
-			console.log("change");
-		},
 		render: function() {
-
+			for (var key in this.boxes) {
+				var currentOption = this.model.get('active');
+				var isSelected = (currentOption && currentOption.get('value') === key);
+				this.boxes[key].attr({
+					fill: isSelected ? this.options["selectedBoxColor"] : this.options["notSelectedBoxColor"]
+				});
+			}
 		}
 	});
 
@@ -120,50 +103,61 @@ $(document).ready(function() {
 	var height = 1024 / 2;
 	var canvas = Raphael("canvas", width, height);
 
-	var Application = Backbone.Controller.extend({
-		paper: canvas,
-		routes: {
-			"combat": "combatAction"
-		},
-		combatAction: function() {
-			console.log("foobar");
-		}
-	});
-
-	AppView = Backbone.View.extend({
-		el: $("body"),
-		initialize: function() {
-			this.weaponTypes = weaponTypes;
-		}
-	});
-
-	var options = new Options;
-	options.add(new Option({
-		value: 1,
+	var lowlightOptions = new Options;
+	lowlightOptions.add(new Option({
+		value: "natural",
 		path: "M16,8.286C8.454,8.286,2.5,16,2.5,16s5.954,7.715,13.5,7.715c5.771,0,13.5-7.715,13.5-7.715S21.771,8.286,16,8.286zM16,20.807c-2.649,0-4.807-2.156-4.807-4.807c0-2.65,2.158-4.807,4.807-4.807c2.648,0,4.807,2.158,4.807,4.807C20.807,18.648,18.648,20.807,16,20.807zM16,13.194c-1.549,0-2.806,1.256-2.806,2.806S14.45,18.807,16,18.807S18.807,17.55,18.807,16C18.807,14.451,17.55,13.194,16,13.194z"
 	}), {
 		silent: true
 	});
-	options.add(new Option({
-		value: 2,
-		path: "M16,8.286C8.454,8.286,2.5,16,2.5,16s5.954,7.715,13.5,7.715c5.771,0,13.5-7.715,13.5-7.715S21.771,8.286,16,8.286zM16,20.807c-2.649,0-4.807-2.156-4.807-4.807c0-2.65,2.158-4.807,4.807-4.807c2.648,0,4.807,2.158,4.807,4.807C20.807,18.648,18.648,20.807,16,20.807zM16,13.194c-1.549,0-2.806,1.256-2.806,2.806S14.45,18.807,16,18.807S18.807,17.55,18.807,16C18.807,14.451,17.55,13.194,16,13.194z"
+	lowlightOptions.add(new Option({
+		value: "cyberware",
+		path: "M 16,8.286 C 8.454,8.286 2.5,16 2.5,16 2.5,16 8.454,23.715 16,23.715 21.771,23.715 29.5,16 29.5,16 29.5,16 21.771,8.286 16,8.286 z m 0,12.521 c -2.649,0 -4.807,-2.156 -4.807,-4.807 0,-2.65 2.158,-4.807 4.807,-4.807 2.648,0 4.807,2.158 4.807,4.807 0,2.648 -2.159,4.807 -4.807,4.807 z m 0,-7.613 c -1.549,0 -2.806,1.256 -2.806,2.806 0,1.55 1.256,2.807 2.806,2.807 1.55,0 2.807,-1.257 2.807,-2.807 0,-1.549 -1.257,-2.806 -2.807,-2.806 z m 9.303912,-11.0687023 0,3 -3,0 0,2 3,0 0,3.0000003 2,0 0,-3.0000003 3,0 0,-2 -3,0 0,-3 z"
 	}), {
 		silent: true
 	});
 
-	var optionGroup = new OptionGroup({
+	var lowlightOptionGroup = new OptionGroup({
 		"id": "test",
-		"options": options
+		"options": lowlightOptions
 	});
 
-	var stuff = new OptionGroupView({
-		model: optionGroup,
+	var lowlightView = new OptionGroupView({
+		model: lowlightOptionGroup,
 		paper: canvas
 	});
-	stuff.initialize();
+	lowlightView.initialize();
 
-	//Initiate a new history and controller class
-	Backbone.emulateHTTP = true;
-	Backbone.emulateJSON = true;
+	// normally you would pass the changed attribute as the second parameter
+	// but render method can't with undefined values, so we leave it out 
+	lowlightOptionGroup.bind('change:active', function(model) {
+	 	lowlightView.render();
+	});
+
+	$(".picker").bind('click', function(){
+		var classes = $(this).attr("class");
+		var optionGroup = widgets[getOptionCharacteristic(classes, 'id.')];
+		var value = getOptionCharacteristic(classes, 'value.');
+		var selectedOption = optionGroup.get('options').select(function(o){ return o.get('value') === value	; })[0];
+		var currentOption =  optionGroup.get('active');
+		if (currentOption && currentOption.get('value') === value) {
+			optionGroup.set({'active': undefined});
+		} else {
+			optionGroup.set({'active': selectedOption});
+		}
+	});
+	
+	var widgets = {};
+	widgets["test"] = lowlightOptionGroup;
+	
+	function getOptionCharacteristic(classes, characteristic) {
+		var c = classes.split(' ');
+		for (var i = 0, length = c.length; i < length; ++i) {
+			if (_(c[i]).startsWith(characteristic)) {
+				return c[i].substring(characteristic.length);
+			}
+		}
+		return undefined;
+	}
 
 });
